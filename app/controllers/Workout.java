dc2460b4fa908bb00cffe52c686db65aa3bd0aa6
bradlund <lundbc@gmail.com>
2012-02-29 08:51:53 -0500

    add a new path, /Workout/index

---
+++ app/controllers/Workout.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: brad
+ * Date: 2/29/12
+ * Time: 8:44 AM
+ */
+public class Workout extends Controller {
+
+	public static void index() {
+		render();
+	}
+}
