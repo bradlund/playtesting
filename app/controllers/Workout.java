a5ccd3a5443160d799af9393497188e5151338b6
bradlund <lundbc@gmail.com>
2012-02-29 12:17:17 -0500

    add link through to workout page

--- app/controllers/Workout.java
+++ app/controllers/Workout.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import models.executions.Exercise;
+import models.executions.Session;
 import play.mvc.Controller;
 
 /**
@@ -10,7 +12,40 @@ import play.mvc.Controller;
  */
 public class Workout extends Controller {
 
-	public static void index() {
+/*	public static void index() {
 		render();
+	}*/
+
+	/**
+	 * If the user is launching into a new workout session, find the first exercise
+	 * and use that as the first exercise to display.
+	 *
+	 * @param sessionId
+	 */
+	public static void workoutBySession(Long sessionId) {
+
+		Session session = Session.find("byId", sessionId).first();
+		Exercise firstExercise;
+
+		if (null != session && session.exercises.size() > 0) {
+			firstExercise = session.exercises.get(0);
+			workoutBySessionAndExercise( sessionId, firstExercise.id);
+		}
+		else
+		{
+			workoutBySessionAndExercise( sessionId, 0L);
+		}
+	}
+
+	/**
+	 * Display the current and upcoming exercises.
+	 *
+	 * @param sessionId
+	 * @param exerciseId
+	 */
+	public static void workoutBySessionAndExercise(Long sessionId, Long exerciseId) {
+		Session session = Session.find("byId", sessionId).first();
+		Exercise exercise = Exercise.find("byId", exerciseId).first();
+		render(session, exercise);
 	}
 }
